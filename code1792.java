1717359c82c489e3f72a4f64787288ee1670c286
sudhakar_D <sudhakarab360@gmail.com>
2025-09-05 08:11:43 +0530

    commit

--- code1792.java
+++ code1792.java
@@ -3,37 +3,32 @@ import java.util.Collections;
 
 public class code1792 {
     public static void main(String[] args) {
-        int classes[][] = {{1,2},{3,5},{2,2}};
-        int extraStudents = 2;
-        int len = classes.length;
-        // float value[]=new float[len];
-        ArrayList<Double>value=new ArrayList<>();
-        System.out.println(classes.length);
-        int k=0;
-        for(int i=0;i<len;i++)
-        {
-            boolean flag=false;
-            Float tot = (float) 0 ;
-            for(int j=0;j<classes.length;j++)
-            {
-                
-                if(i==j)
-                {
-                       
-                        tot=(float) (tot+ ((((classes[j][0])+extraStudents)*1.0)/(classes[j][1]+extraStudents)));
-                       
-                }
-                else
-                {
-                   
-                      tot=(float) ((float) tot+(((classes[j][0]))*1.0)/(classes[j][1]));
-                     
-                }
-            }
-            value.add((double) (tot/3));
-        }
-        System.out.println(Collections.max(value));
+        // int classes[][] = {{1,2},{3,5},{2,2}};
+        // int extraStudents = 2;
 
+        int classes[][] = {{2,4},{3,9},{4,5},{2,10}};
+        int extraStudents = 4;
+
+
+        double ans=0;
+        System.out.println(1/2);
+        for(int i=0;i<classes.length;i++)
+        {
+            Double fl;
+            Double max=(double) 0;
+            for(int j=0;j<extraStudents;j++)
+            {
+                 System.out.println("ff"+(classes[i][0]+j)+" "+(classes[i][1]+j));
+                 fl= ((double)(classes[i][0]+j)/((double)classes[i][1]+j));
+                 max=Math.max(max, fl);
+                 
+                 System.out.println("--"+fl);
+            }
+            System.out.println("MAX"+max);
+            ans+=max;
+        }
+        System.out.println((double) ans/3);
+        System.out.printf("%.5f\n", ans/3);
 
 
     }
---
+++ code3516.java
@@ -0,0 +1,17 @@
+public class code3516 {
+    public static void main(String[] args) {
+        int x = 10, y = 1, z = 12;
+        int step1,step2;
+        step1=z-x;
+        step2=y-z;
+        System.out.println(step1+" "+step2);
+        if(step1<step2)
+        {
+            System.out.println("1");
+        }
+        else
+        {
+            System.out.println("2");
+        }
+    }
+}
